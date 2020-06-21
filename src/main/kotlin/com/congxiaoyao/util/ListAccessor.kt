290df35f35ae871e86ff3a21984d592cc6d29c92
duhongliu <duhongliu@mafengwo.com>
2020-06-21 19:11:30 +0800

    [commit]add access method

--- src/main/kotlin/com/congxiaoyao/util/ListAccessor.kt
+++ src/main/kotlin/com/congxiaoyao/util/ListAccessor.kt
@@ -12,7 +12,7 @@ open class ListAccessor<T, A : Kind<ListAccessor.K, T>>(
     fun get(next: Int) = entities[pointer + next]
     fun moveToNext() = ++pointer
     fun moveToLast() = --pointer
-    fun canAccess() = pointer < entities.size && pointer > 0
+    fun canAccess() = pointer in 0..end
 
     inline fun accessByRange(start: Int, end: Int, action: AccessCallback<A>) {
         val direction = Direction.valueOf(end - start).value()
@@ -31,18 +31,42 @@ open class ListAccessor<T, A : Kind<ListAccessor.K, T>>(
         accessByRange(start, endIndex, action)
     }
 
+
+    fun accessEach(direction: Direction, action: AccessCallback<A>) {
+        for (i in if(direction is Direction.Pos) 0..end else end downTo 0) {
+            pointer = i
+            action(unwrap(), i, direction.value())
+        }
+    }
+
+    fun accessToEdge(direction: Direction, index: Int, action: AccessCallback<A>) {
+        if (index !in 0..end) return
+        for (i in if(direction is Direction.Pos) index..end else index downTo 0) {
+            pointer = i
+            action(unwrap(), i, direction.value())
+        }
+    }
+
+    fun accessFromEdge(direction: Direction, index: Int, action: AccessCallback<A>) {
+        if (index !in 0..end) return
+        for (i in if(direction is Direction.Pos) 0..index else end downTo index) {
+            pointer = i
+            action(unwrap(), i, direction.value())
+        }
+    }
+
     fun unwrap() = this as A
 
     sealed class Direction {
         companion object {
-            fun valueOf(value: Int) = if (value < 0) Nag else Pos
+            fun valueOf(value: Int) = if (value < 0) Neg else Pos
         }
         abstract fun value(): Int
 
         object Pos : Direction() {
             override fun value() = 1
         }
-        object Nag : Direction() {
+        object Neg : Direction() {
             override fun value() = -1
         }
     }
@@ -50,6 +74,7 @@ open class ListAccessor<T, A : Kind<ListAccessor.K, T>>(
 private typealias AccessCallback<A> = ((aac: A, gen: Int, dir: Int) -> Unit)
 
 class ReadableListAccessor<T>(entities: List<T>) : ListAccessor<T, ReadableListAccessor<T>>(entities)
+
 class WriteableListAccessor<T>(val entities: MutableList<T>) : ListAccessor<T, WriteableListAccessor<T>>(entities) {
     fun set(value: T) = set(0, value)
     fun set(next: Int, value: T) = entities.set(pointer + next, value)
